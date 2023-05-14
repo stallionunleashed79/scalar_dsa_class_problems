e994f33dcee156b7fbe6c6616549f8e84a0d7d4f
Bharath Seshadri <chasethesky79@gmail.com>
2023-05-14 11:30:31 -0500

    Implemented solution to rotate array clockwise and anti-clockwise

--- src/com/introtoarrays/RotateAGivenArrayKTimes.java
+++ src/com/introtoarrays/RotateAGivenArrayKTimes.java
@@ -5,26 +5,43 @@ import com.util.ArrayUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-// GIVEN AN ARRAY OF N ELEMENTS, ROTATE IT K NUMBER OF TIMES
+// GIVEN AN ARRAY OF N ELEMENTS, ROTATE IT K NUMBER OF TIMES BOTH CLOCKWISE AND ANTI-CLOCKWISE
 public class RotateAGivenArrayKTimes {
 
     public static void main(String[] args) {
-        rotateAndPrint(new ArrayList<>(List.of( 3, -2, 1, 4, 6, 9, 8)), 3);
-        rotateAndPrint(new ArrayList<>(List.of( 3, -2, 1, 4, 6, 9, 8)), 8);
-        rotateAndPrint(new ArrayList<>(List.of( 4, 1, 6, 9, 2, 14, 7, 8, 3)), 4);
+        // ROTATE CLOCKWISE
+        rotateAntiClockwiseAndPrint(new ArrayList<>(List.of( 3, -2, 1, 4, 6, 9, 8)), 3);
+        rotateAntiClockwiseAndPrint(new ArrayList<>(List.of( 3, -2, 1, 4, 6, 9, 8)), 8);
+        rotateAntiClockwiseAndPrint(new ArrayList<>(List.of( 1, 2, 3, 4, 5)), 2);
+
+        // ROTATE ANI-CLOCKWISE
+        rotateClockwiseAndPrint(new ArrayList<>(List.of( 3, -2, 1, 4, 6, 9, 8)), 3);
+        rotateClockwiseAndPrint(new ArrayList<>(List.of( 3, -2, 1, 4, 6, 9, 8)), 8);
+        rotateClockwiseAndPrint(new ArrayList<>(List.of( 1, 2, 3, 4, 5)), 2);
     }
 
-    private static void rotateAndPrint(final List<Integer> input, int K) {
+    private static void rotateAntiClockwiseAndPrint(final List<Integer> input, int K) {
         K = K % input.size();
-        rotateArrayKTimes(input, K);
-        System.out.println("ROTATED ARRAY "+ input);
+        rotateArrayKTimesAntiClockwise(input, K);
+        System.out.println("ROTATED ARRAY ANTI-CLOCKWISE "+ input);
     }
 
-    private static void rotateArrayKTimes(final List<Integer> input, final int K) {
-        ArrayUtils.reversePartOfArray(input, 0, input.size() - K - 1);
-        ArrayUtils.reversePartOfArray(input, input.size() - K, input.size() - 1);
+    private static void rotateClockwiseAndPrint(final List<Integer> input, int K) {
+        K = K % input.size();
+        rotateArrayKTimesClockwise(input, K);
+        System.out.println("ROTATED ARRAY CLOCKWISE "+ input);
+    }
+
+    private static void rotateArrayKTimesAntiClockwise(final List<Integer> input, final int K) {
         ArrayUtils.reversePartOfArray(input, 0, input.size() - 1);
+        ArrayUtils.reversePartOfArray(input, 0,  K - 1);
+        ArrayUtils.reversePartOfArray(input, K, input.size() - 1);
     }
 
+    private static void rotateArrayKTimesClockwise(final List<Integer> input, final int K) {
+        ArrayUtils.reversePartOfArray(input, 0, input.size() - 1);
+        ArrayUtils.reversePartOfArray(input, 0,  input.size() - K - 1);
+        ArrayUtils.reversePartOfArray(input, input.size() - K, input.size() - 1);
+    }
 
 }
