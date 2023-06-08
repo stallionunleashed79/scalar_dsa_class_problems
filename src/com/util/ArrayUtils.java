1f34fd9efceac299b325451a253ab4411c3c119f
Bharath Seshadri <chasethesky79@gmail.com>
2023-06-08 10:53:24 -0500

    Implemented prefix sum even array

--- src/com/util/ArrayUtils.java
+++ src/com/util/ArrayUtils.java
@@ -15,6 +15,16 @@ public class ArrayUtils {
         return prefixSumArray;
     }
 
+    // TC = O(N) and SC = O(N) due to the new even PFSum array
+    public static List<Integer> buildEvenPrefixSumArray(final List<Integer> input) {
+        final List<Integer> prefixSumArray = new ArrayList<>();
+        prefixSumArray.add(input.get(0));
+        for (int i =1; i < input.size(); i++) {
+            prefixSumArray.add(Integer.sum(i % 2 == 0 ? input.get(i) : 0, prefixSumArray.get(i-1)));
+        }
+        return prefixSumArray;
+    }
+
     public static void reversePartOfArray(final List<Integer> input, final int startIndex, final int endIndex) {
         for (int i =startIndex, j = endIndex; i <= j; i++, j--) {
             Integer temp = input.get(i);
