084b5ab231f124ad294631ed47b239e2165045ef
chasethesky79 <seshadri.bharath@yahoo.com>
2023-06-28 17:16:56 -0500

    Added description for transpose of a 2D rectangular matrix

--- src/com/twodimensionalarrays/GivenA2DRectangularMatrixTransposeIt.java
+++ src/com/twodimensionalarrays/GivenA2DRectangularMatrixTransposeIt.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 
 public class GivenA2DRectangularMatrixTransposeIt {
 
+    // TC = O(N*M) SINCE WE ARE TOUCHING ALL CELLS OF MATRIX AND SC = O(N*M) DUE TO CREATING AN EXTRA 2D RECTANGULAR
+    // MATRIX TO HOLD THE RESULTS AS A RECTANGULAR MATRIX CANNOT BE TRANSPOSED IN-PLACE
     public static void main(String[] args) {
         System.out.println("TRANSPOSED SQUARE MATRIX IS " + ArrayUtils.getTransposeOf2DRectangularMatrix(Arrays.asList(
                 Arrays.asList( 1, 2, 3 ), Arrays.asList( 4, 5, 6 ))));
