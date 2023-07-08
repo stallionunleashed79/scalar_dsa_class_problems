74600e521551f1bb07a3e5d23d7c1ee42c461a4a
chasethesky79 <seshadri.bharath@yahoo.com>
2023-07-08 13:49:47 -0500

    Implemented solution to print start and end indices of subarray of size k

---
+++ src/com/subarrays/PrintStartAndEndIndexOfSubArrayLengthK.java
@@ -0,0 +1,20 @@
+package com.subarrays;
+
+import java.util.List;
+
+public class PrintStartAndEndIndexOfSubArrayLengthK {
+
+    public static void main(String[] args) {
+        System.out.println("PRINTING START AND END INDEX OF SUB-ARRAYS OF LENGTH ");
+        printStartAndEndIndexOfSubArrayLengthK(List.of( 7, 3, 2, -1, 6, 8, 2, 3 ), 4);
+        System.out.println();
+        printStartAndEndIndexOfSubArrayLengthK(List.of( 3, 4, 2, -1, 6, 7, 8, 9, 3, 2, -1, 4 ), 3);
+    }
+
+    private static void printStartAndEndIndexOfSubArrayLengthK(final List<Integer> input, final int K) {
+        int N = input.size();
+        for (int i = 0; i <= N-K; i++) {
+            System.out.println("START AND END INDEX "+ i +" "+(i + K-1));
+        }
+    }
+}
