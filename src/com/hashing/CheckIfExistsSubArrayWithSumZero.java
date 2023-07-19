1b0109e2d583f99a27076e23a73905193b530617
chasethesky79 <seshadri.bharath@yahoo.com>
2023-07-19 12:31:41 -0500

    Fixed edge case for sub array sum

--- src/com/hashing/CheckIfExistsSubArrayWithSumZero.java
+++ src/com/hashing/CheckIfExistsSubArrayWithSumZero.java
@@ -12,6 +12,7 @@ public class CheckIfExistsSubArrayWithSumZero {
     public static void main(String[] args) {
         System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArray(List.of(2, 2, 1, -3, 4, 3, 1, -2, -3, 2)));
         System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArray(List.of(6, 6, 1, -3, 4, 3, 1, -2, -3, 2)));
+        System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArray(List.of(2, -2)));
     }
 
     // TC = O(N) and SC = O(N) FOR PREFIX SUM ARRAY, BUILD A PREFIX SUM ARRAY FOR GIVEN ARRAY AND CHECK IF THERE ARE DUPLICATES IN
@@ -20,7 +21,7 @@ public class CheckIfExistsSubArrayWithSumZero {
         final List<Integer> prefixSumArray = ArrayUtils.buildPrefixSumArray(input);
         final Set<Integer> set = new HashSet<>();
         for (Integer element: prefixSumArray) {
-            if (!set.add(element)) {
+            if (element.compareTo(0) == 0 || !set.add(element)) {
                 return true;
             }
         }
