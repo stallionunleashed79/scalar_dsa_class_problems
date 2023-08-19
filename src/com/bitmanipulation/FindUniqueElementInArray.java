066ea6f5abc34ca076d7a336b246790f6e8efb26
chasethesky79 <seshadri.bharath@yahoo.com>
2023-08-19 11:33:39 -0500

    Added description for unique element in array

--- src/com/bitmanipulation/FindUniqueElementInArray.java
+++ src/com/bitmanipulation/FindUniqueElementInArray.java
@@ -7,14 +7,24 @@ public class FindUniqueElementInArray {
 
     public static void main(String[] args) {
         System.out.println("FIND UNIQUE ELEMENT FOR ARRAY "+findUniqueElement(new int[]{ 2, 3, 2, 4, 3, 4, 5 })); // Each number repeats twice except one number
+        System.out.println("FIND UNIQUE ELEMENT FOR ARRAY "+findUniqueElementAlternativeSolution(new int[]{ 2, 3, 2, 4, 3, 4, 5 })); // Each number repeats twice except one number
         System.out.println("FIND UNIQUE ELEMENT FOR ARRAY "+findUniqueElement(new int[]{ 2, 2, 7, 9, 7, 2, 7, 2, 7 })); // Each number repeats four times except one number
         System.out.println("FIND UNIQUE ELEMENT FOR ARRAY "+findUniqueElement(new int[]{ 2, 2, 7, 9, 7, 2 })); // XOR SOLUTION DOES NOT WORK HERE
         // Each number repeats three times except one number (IN THIS CASE XOR DOES NOT WORK) - HERE 2 REPEATS THREE TIMES SO XOR SOLUTION NOT WORKS
     }
 
+    // TC = O(N) due to iterating the for loop N times and SC = O(1) due to no extra space
     private static int findUniqueElement(int[] input) {
         int answer = 0;
         for (int i = 0; i < input.length; i++) {
+            answer = answer  ^ input[i];
+        }
+        return answer;
+    }
+
+    private static int findUniqueElementAlternativeSolution(int[] input) {
+        int answer = input[0];
+        for (int i = 1; i < input.length; i++) {
             answer = answer  ^ input[i];
         }
         return answer;
