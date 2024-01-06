4749328805302e43121fe4235ba19abcc5929fa4
chasethesky79 <seshadri.bharath@yahoo.com>
2024-01-06 14:39:41 -0600

    Renamed solution methods to reflect intent

--- src/com/carryforward/CountPossiblePairsInGivenArray.java
+++ src/com/carryforward/CountPossiblePairsInGivenArray.java
@@ -7,11 +7,11 @@ public class CountPossiblePairsInGivenArray {
     public static void main(String[] args) {
         System.out.println("COUNT NUMBER OF 'AG' PAIRS IN ARRAY "+ countNumberOfPairsBruteForce(
                 new char[]{ 'b', 'a', 'a', 'g', 'd', 'c', 'a', 'g' }));
-        System.out.println("COUNT NUMBER OF 'AG' PAIRS IN ARRAY "+ countNumberOfPairsOptimized(
+        System.out.println("COUNT NUMBER OF 'AG' PAIRS IN ARRAY "+ countNumberOfPairsOptimizedFromEnd(
                 new char[]{ 'b', 'a', 'a', 'g', 'd', 'c', 'a', 'g' }));
-        System.out.println("COUNT NUMBER OF 'AG' PAIRS IN ARRAY "+ countNumberOfPairsOptimized(
+        System.out.println("COUNT NUMBER OF 'AG' PAIRS IN ARRAY "+ countNumberOfPairsOptimizedFromEnd(
                 new char[]{ 'a', 'd', 'g', 'a', 'g', 'a', 'g', 'f', 'g' }));
-        System.out.println("COUNT NUMBER OF 'AG' PAIRS IN ARRAY "+ countNumberOfPairsOptimizedSecondSolution(
+        System.out.println("COUNT NUMBER OF 'AG' PAIRS IN ARRAY "+ countNumberOfPairsOptimizedFromBeginning(
                 new char[]{ 'a', 'd', 'g', 'a', 'g', 'a', 'g', 'f', 'g' }));
     }
     // TC = O(N^2) and SC = O(1) since no extra space is used
@@ -31,7 +31,7 @@ public class CountPossiblePairsInGivenArray {
     }
 
     // TC = O(N) and SC = O(1)
-    private static int countNumberOfPairsOptimized(final char[] input) {
+    private static int countNumberOfPairsOptimizedFromEnd(final char[] input) {
         int answer = 0;
         int gCount = 0;
         for (int i = input.length-1; i >=0 ; i--) {
@@ -44,14 +44,14 @@ public class CountPossiblePairsInGivenArray {
         return answer;
     }
 
-    private static int countNumberOfPairsOptimizedSecondSolution(final char[] input) {
+    private static int countNumberOfPairsOptimizedFromBeginning(final char[] input) {
         int answer = 0;
-        int gCount = 0;
+        int aCount = 0;
         for (int i = 0; i < input.length ; i++) {
             if (input[i] == 'a') {
-                gCount++;
+                aCount++;
             } else if (input[i] == 'g') {
-                answer = answer + gCount;
+                answer = answer + aCount;
             }
         }
         return answer;
