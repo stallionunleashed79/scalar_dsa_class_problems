85d3765e3adbca14ced9ab91f5869a510aaea8df
chasethesky79 <seshadri.bharath@yahoo.com>
2024-07-28 11:14:20 -0500

    Implemented number of two sum pairs with hashset

--- src/com/hashing/TowSumProblemWithHashSet.java
+++ src/com/hashing/TowSumProblemWithHashSet.java
@@ -1,8 +1,6 @@
 package com.hashing;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 // GIVEN AN ARRAY OF SIZE N, RETURN 2 VALUES FROM ARRAY SUCH THAT THE SUM
 // OF ELEMENTS AT THOSE 2 INDICES EQUALS A GIVEN TARGET K ELSE RETURN EMPTY ARRAY
@@ -13,6 +11,10 @@ public class TowSumProblemWithHashSet {
                 List.of( 8, 9, 1, -2, 4, 5, 11, -6, 7, 5 ), 11)));
         System.out.println("FIRST PAIR OF ELEMENTS SUMMING TO K EQUALS "+ Arrays.toString(getFirstPairOfValuesSummingToKOptimized(
                 List.of( 8, 9, 1, -2, 4, 5, 11, -6, 7, 5 ), 22)));
+        System.out.println("NUMBER OF PAIRS OF ELEMENTS SUMMING TO K EQUALS "+ getNumberOfPairsWhoseSumIsEqualToTargetSum(
+                List.of( 8, 9, 1, -2, 4, 5, 11, -6, 7, 5 ), 22));
+        System.out.println("NUMBER OF PAIRS OF ELEMENTS SUMMING TO K EQUALS "+ getNumberOfPairsWhoseSumIsEqualToTargetSum(
+                List.of( 8, 9, 1, -2, 4, 5, 11, -6, 7, 5 ), 11));
     }
     // TC = O(N) and SC = O(N) for the extra hashset
     private static Integer[] getFirstPairOfValuesSummingToKOptimized(final List<Integer> input, final Integer K) {
@@ -26,5 +28,19 @@ public class TowSumProblemWithHashSet {
             }
         }
         return new Integer[]{};
+    }
+
+    private static Integer getNumberOfPairsWhoseSumIsEqualToTargetSum(final List<Integer> input, final Integer K) {
+        int answer = 0;
+        final HashSet<Integer> hashSet = new HashSet<>();
+        for (Integer element: input) {
+            int difference = K - element;
+            if (!hashSet.contains(difference)) {
+                hashSet.add(element);
+            } else {
+                answer++;
+            }
+        }
+        return answer;
     }
 }
