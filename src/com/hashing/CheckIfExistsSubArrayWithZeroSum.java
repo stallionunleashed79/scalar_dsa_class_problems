d23686d680e442fa57c8dd1848401df974ce7675
chasethesky79 <seshadri.bharath@yahoo.com>
2024-08-03 17:34:45 -0500

    Implemented optimized solution for zero sum subarray

--- src/com/hashing/CheckIfExistsSubArrayWithZeroSum.java
+++ src/com/hashing/CheckIfExistsSubArrayWithZeroSum.java
@@ -14,6 +14,10 @@ public class CheckIfExistsSubArrayWithZeroSum {
         System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArray(List.of(2, 2, 1, -3, 4, 3, 1, -2, -3, 2)));
         System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArray(List.of(6, 6, 1, -3, 4, 3, 1, -2, -3, 2)));
         System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArray(List.of(2, -2)));
+
+        System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArrayInASingleLoop(List.of(2, 2, 1, -3, 4, 3, 1, -2, -3, 2)));
+        System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArrayInASingleLoop(List.of(6, 6, 1, -3, 4, 3, 1, -2, -3, 2)));
+        System.out.println("DOES EXIST A ZERO SUM SUBARRAY FOR "+doesExistZeroSumSubArrayInASingleLoop(List.of(2, -2)));
     }
 
     // TC = O(N) and SC = O(N) FOR PREFIX SUM ARRAY, BUILD A PREFIX SUM ARRAY FOR GIVEN ARRAY AND CHECK IF THERE ARE DUPLICATES IN
@@ -25,6 +29,24 @@ public class CheckIfExistsSubArrayWithZeroSum {
             if (element.compareTo(0) == 0 || !set.add(element)) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    /**
+     * TC = O(N) due to a single for loop and SC = O(N) due to use of hashset
+     * @param input
+     * @return
+     */
+    private static boolean doesExistZeroSumSubArrayInASingleLoop(final List<Integer> input) {
+        Integer pfSum = 0;
+        final Set<Integer> set = new HashSet<>();
+        for (Integer element: input) {
+            pfSum = Integer.sum(pfSum, element);
+            if (pfSum.compareTo(0) == 0 || set.contains(pfSum)) {
+                return true;
+            }
+            set.add(pfSum);
         }
         return false;
     }
