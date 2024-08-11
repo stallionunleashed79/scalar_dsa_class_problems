9d86307c2172ca06b4c509a9ac918d4e4cc76129
chasethesky79 <seshadri.bharath@yahoo.com>
2024-08-11 17:38:29 -0500

    Fixed logic that computes sum of all subarray sums

--- src/com/subarrays/FindSumOfAllSubArraySums.java
+++ src/com/subarrays/FindSumOfAllSubArraySums.java
@@ -4,13 +4,14 @@ public class FindSumOfAllSubArraySums {
 
     public static void main(String[] args) {
        System.out.println("SUM OF ALL SUBARRAY SUMS "+ sumOfAllSubArraySums(new int[]{ 4, 3, 1, 2, 5, 7}));
+        System.out.println("SUM OF ALL SUBARRAY SUMS "+ sumOfAllSubArraySums(new int[]{ 3, 2, 5 }));
     }
     // TC = O(N), SC = O(1) - Optimized solution
     private static long sumOfAllSubArraySums(int[] A) {
         long total = 0;
         int N = A.length;
         for (int i =0; i < N; i++) {
-            total = total + (long)i*(i+1)*(N-i);
+            total = total + (long) A[i] *(i+1)*(N-i);
         }
         return total;
     }
