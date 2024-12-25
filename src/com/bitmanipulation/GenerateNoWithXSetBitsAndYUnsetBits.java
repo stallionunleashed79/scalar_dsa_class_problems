525b4b068f1ccbf590293204441e9bc5e8716cb0
chasethesky79 <seshadri.bharath@yahoo.com>
2024-12-25 14:46:01 -0600

    Implemented problem to generate numbe after set x bits and unset y bits

--- src/com/bitmanipulation/GenerateNoWithXSetBitsAndYUnsetBits.java
+++ src/com/bitmanipulation/GenerateNoWithXSetBitsAndYUnsetBits.java
@@ -8,11 +8,21 @@ public class GenerateNoWithXSetBitsAndYUnsetBits {
     public static void main (String[] args) {
         System.out.println("GENERATE NUMBER AFTER SETTING X BITS AND UNSETING Y BITS " + setXBitsAndUnsetYBits(3, 2));
         System.out.println("GENERATE NUMBER AFTER SETTING X BITS AND UNSETING Y BITS " + setXBitsAndUnsetYBits(2, 3));
+        System.out.println("GENERATE NUMBER AFTER SETTING X BITS " + setXBits(2));
+        System.out.println("GENERATE NUMBER AFTER SETTING X BITS " + setXBits(3));
     }
 
     private static int setXBitsAndUnsetYBits(int x, int y) {
         int ans = 0;
         for (int i = x; i < x + y; i++) {
+            ans = ans + (1 << i);
+        }
+        return ans;
+    }
+
+    private static int setXBits(int x) {
+        int ans = 0;
+        for (int i = 0; i < x; i++) {
             ans = ans + (1 << i);
         }
         return ans;
