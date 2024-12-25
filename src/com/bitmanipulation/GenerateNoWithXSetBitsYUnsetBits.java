9aa7bf5013ce1e0dc7c41c12e1e24c013a065f86
chasethesky79 <seshadri.bharath@yahoo.com>
2024-12-25 14:56:47 -0600

    Implemented problem to generate numbe after set x bits and unset y bits alternative and optimized

--- src/com/bitmanipulation/GenerateNoWithXSetBitsYUnsetBits.java
+++ src/com/bitmanipulation/GenerateNoWithXSetBitsYUnsetBits.java
@@ -8,9 +8,20 @@ public class GenerateNoWithXSetBitsYUnsetBits {
 
         System.out.println("NUMBER GENERATED AFTER X SET BITS AND Y UNSET BITS "+ generateAlternative(2, 3));
         System.out.println("NUMBER GENERATED AFTER X SET BITS AND Y UNSET BITS "+ generateAlternative(3, 2));
+
+        System.out.println("NUMBER GENERATED AFTER X SET BITS AND Y UNSET BITS "+ generateOptimized(2, 3));
+        System.out.println("NUMBER GENERATED AFTER X SET BITS AND Y UNSET BITS "+ generateOptimized(3, 2));
+    }
+
+    private static int generate(int x, int y) {
+        int ans = 0;
+        for (int i = y; i < x + y; i++) {
+            ans = ans + (1 << i);
+        }
+        return ans;
     }
     // TC = O(x) where x is number of bits to set and SC = O(1) since no extra space used
-    private static int generate(int x, int y) {
+    private static int generateAlternative(int x, int y) {
         int n = 0;
         for (int i = y; i < y + x; i++) {
             n = n | (1 << i);
@@ -18,7 +29,7 @@ public class GenerateNoWithXSetBitsYUnsetBits {
         return n;
     }
 
-    private static int generateAlternative(int x, int y) {
+    private static int generateOptimized(int x, int y) {
         return ((1 << x) - 1) << y;
     }
 }
