ee65fc50ea350085edb09c573d3cf87bcfc10eb5
chasethesky79 <seshadri.bharath@yahoo.com>
2025-01-11 11:40:50 -0600

    Implemented power function optimized

--- src/com/recursion/PowerFunction.java
+++ src/com/recursion/PowerFunction.java
@@ -6,13 +6,33 @@ package com.recursion;
 public class PowerFunction {
 
     public static void main(String[] args) {
-        System.out.println("POWER FUNCTION "+ power(2, 3));
-        System.out.println("POWER FUNCTION "+ power(3, 4));
+        System.out.println("POWER FUNCTION "+ powerFunction(2, 3));
+        System.out.println("POWER FUNCTION "+ powerFunction(3, 4));
+        System.out.println("POWER FUNCTION "+ powerFunctionOptimized(2, 3));
+        System.out.println("POWER FUNCTION "+ powerFunctionOptimized(3, 4));
     }
-    private static int power(int a, int N) {
+
+    /**
+     * Time complexity is O(N)
+     * @param a
+     * @param N
+     * @return
+     */
+    private static int powerFunction(int a, int N) {
         if (N == 0) {
             return 1;
         }
-        return a * power(a, N-1);
+        return a * powerFunction(a, N-1);
+    }
+
+    private static int powerFunctionOptimized(int a, int N) {
+        if (N == 0) {
+            return 1;
+        }
+        int halfPower = powerFunctionOptimized(a, N/2);
+        if (N % 2 == 0) {
+            return halfPower * halfPower;
+        }
+        return halfPower * halfPower * a;
     }
 }
