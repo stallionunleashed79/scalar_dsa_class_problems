38203863d070dbaa3887be28829528488f241809
chasethesky79 <seshadri.bharath@yahoo.com>
2025-01-13 21:50:21 -0600

    Implemented the merge intervals problem

---
+++ src/com/introtoarrays/MergeIntervals.java
@@ -0,0 +1,56 @@
+package com.introtoarrays;
+
+import com.util.Interval;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+
+    public static void main(String[] args) {
+        final Interval interval1 = new Interval();
+        interval1.setStart(0);
+        interval1.setEnd(2);
+        final Interval interval2 = new Interval();
+        interval2.setStart(1);
+        interval2.setEnd(4);
+        final Interval interval3 = new Interval();
+        interval3.setStart(3);
+        interval3.setEnd(6);
+        final Interval interval4 = new Interval();
+        interval4.setStart(7);
+        interval4.setEnd(8);
+        final Interval interval5 = new Interval();
+        interval5.setStart(9);
+        interval5.setEnd(10);
+        final List<Interval> input = Arrays.asList(interval1, interval2, interval3, interval4, interval5);
+        final List<Interval> result = mergeSortedIntervals(input);
+        System.out.print("NON MERGED INTERVALS ");
+        System.out.println();
+        result.forEach(System.out::println);
+    }
+
+    private static List<Interval> mergeSortedIntervals(final List<Interval> input) {
+        final List<Interval> nonOverlappingIntervals = new ArrayList<>();
+        Interval current = input.get(0);
+        for (int i = 1; i < input.size(); i++) {
+            final Interval next = input.get(i);
+            if (next.getStart().compareTo(current.getEnd()) > 0) {
+                nonOverlappingIntervals.add(current);
+                current = next;
+            } else {
+                current.setStart(Math.min(current.getStart(), next.getStart()));
+                current.setEnd(Math.max(current.getEnd(), next.getEnd()));
+            }
+        }
+        final Interval prev = nonOverlappingIntervals.get(nonOverlappingIntervals.size() - 1);
+        if (current.getStart().compareTo(prev.getEnd()) <= 0) {
+            prev.setStart(Math.min(current.getStart(), prev.getStart()));
+            prev.setEnd(Math.max(current.getEnd(), prev.getEnd()));
+        } else {
+            nonOverlappingIntervals.add(current);
+        }
+        return nonOverlappingIntervals;
+    }
+}
---
+++ src/com/util/Interval.java
@@ -0,0 +1,30 @@
+package com.util;
+
+public class Interval {
+    private Integer start;
+    private Integer end;
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getEnd() {
+        return end;
+    }
+
+    public void setEnd(Integer end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Interval{" +
+                "start=" + start +
+                ", end=" + end +
+                '}';
+    }
+}
