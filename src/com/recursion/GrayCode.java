5d0284ac52b054a9f3b18aad53eae9b92e371c91
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-02 13:47:59 -0600

    Implemented graycode algorithm

--- src/com/recursion/GrayCode.java
+++ src/com/recursion/GrayCode.java
@@ -1,2 +1,27 @@
-package com.recursion;public class GrayCode {
+package com.recursion;
+
+import com.util.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GrayCode {
+
+    public static void main(String[] args) {
+        System.out.println("GRAY CODE FOR "+ grayCode(2));
+        System.out.println("GRAY CODE FOR "+ grayCode(3));
+    }
+    private static List<Integer> grayCode(int N) {
+        if (N == 1) {
+            return new ArrayList<>(Arrays.asList(0, 1));
+        }
+        final List<Integer> result = grayCode(N-1);
+        final List<Integer> secondPart = new ArrayList<>(result);
+        ArrayUtils.reversePartOfArray(secondPart, 0, secondPart.size() - 1);
+        for (Integer element: secondPart) {
+            result.add((int)(Math.pow(2, N-1)) + element);
+        }
+        return result;
+    }
 }
