d38fa12a940435d62f81399a5d45a956adda26dd
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-09 12:55:57 -0600

    Implemented a stack using an array

--- src/com/stacksandqueues/ImplementAStackUsingAnArray.java
+++ src/com/stacksandqueues/ImplementAStackUsingAnArray.java
@@ -7,7 +7,7 @@ public class ImplementAStackUsingAnArray {
     private static int top = -1;
     private static int[] input = new int[100];
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         push(1);
         push(4);
         push(-1);
@@ -15,9 +15,14 @@ public class ImplementAStackUsingAnArray {
         System.out.println("CURRENT ELEMENT "+ pop());
         push(6);
         System.out.println("CURRENT ELEMENT "+ pop());
+        System.out.println("CURRENT ELEMENT "+ pop());
+        System.out.println("CURRENT ELEMENT "+ pop());
     }
 
-    private static int pop() {
+    private static int pop() throws Exception {
+        if (top == -1) {
+            throw new Exception("Stack is empty so cannot pop any more elements");
+        }
         int temp = input[top];
         top--;
         return temp;
