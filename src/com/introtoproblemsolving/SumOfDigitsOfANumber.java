84769e6c9db6daca158f6e179bfb7cddd0d937fc
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-08 19:51:26 -0600

    Implemented sum of digits of a number

---
+++ src/com/introtoproblemsolving/SumOfDigitsOfANumber.java
@@ -0,0 +1,19 @@
+package com.introtoproblemsolving;
+
+public class SumOfDigitsOfANumber {
+
+    public static void main(String[] args) {
+      System.out.println("SUM OF DIGITS OF THE NUMBER "+ sumOfDigits(221));
+        System.out.println("SUM OF DIGITS OF THE NUMBER "+ sumOfDigits(2678));
+    }
+
+    private static int sumOfDigits(int N) {
+        int sum = 0;
+        while (N > 0) {
+            int remainder = N % 10;
+            sum = sum + remainder;
+            N = N / 10;
+        }
+        return sum;
+    }
+}
