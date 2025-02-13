75040761f6ae637fffb93c28d564695876d9c28a
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-12 21:36:34 -0600

    Implemented alternative recursive solution to print in increasing order

--- src/com/recursion/PrintNumbersInIncreasingOrder.java
+++ src/com/recursion/PrintNumbersInIncreasingOrder.java
@@ -4,6 +4,8 @@ public class PrintNumbersInIncreasingOrder {
 
     public static void main(String[] args) {
         printIncreasing(5);
+        System.out.println();
+        printIncreasingAlternativeSolution(5);
     }
 
     private static void printIncreasing(int n) {
@@ -12,5 +14,18 @@ public class PrintNumbersInIncreasingOrder {
         }
         printIncreasing(n-1);
         System.out.print(n +" ");
+    }
+
+    private static void printIncreasingAlternativeSolution(int input) {
+        printIncreasingAlternativeSolutionHelper(input, 1);
+    }
+
+    private static void printIncreasingAlternativeSolutionHelper(final Integer input, int current) {
+        if (current == input + 1) {
+            return;
+        }
+        System.out.print(current +" ");
+        current = current + 1;
+        printIncreasingAlternativeSolutionHelper(input, current);
     }
 }
