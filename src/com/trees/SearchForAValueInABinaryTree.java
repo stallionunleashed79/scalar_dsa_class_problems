3a8d643ab536bb2573c2be95eb3316e457f06b1c
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-20 23:25:14 -0600

    Implemented search for a value in a binary tree

---
+++ src/com/trees/SearchForAValueInABinaryTree.java
@@ -0,0 +1,33 @@
+package com.trees;
+
+/**
+ * SEARCH FOR A GIVEN VALUE IN A BINARY TREE
+ */
+public class SearchForAValueInABinaryTree {
+
+    public static void main(String[] args) {
+        final TreeNode leaf1 = new TreeNode(3, null, null);
+        final TreeNode leaf2 = new TreeNode(4, null, null);
+        final TreeNode leaf3 = new TreeNode(7, null, null);
+        final TreeNode leaf4 = new TreeNode(8, null, null);
+        final TreeNode leftNode = new TreeNode(5, leaf1, leaf2);
+        final TreeNode rightNode = new TreeNode(6, leaf3, leaf4);
+        final TreeNode root = new TreeNode(1, leftNode, rightNode);
+        System.out.println("DOES NUMBER EXIST "+ searchValue(root, 3));
+        System.out.println("DOES NUMBER EXIST "+ searchValue(root, 1));
+        System.out.println("DOES NUMBER EXIST "+ searchValue(root, 10));
+        System.out.println("DOES NUMBER EXIST "+ searchValue(root, 7));
+        System.out.println("DOES NUMBER EXIST "+ searchValue(root, 8));
+        System.out.println("DOES NUMBER EXIST "+ searchValue(root, 13));
+    }
+
+    private static boolean searchValue(final TreeNode root, final int K) {
+        if (root == null) {
+            return false;
+        }
+        if (root.getData() == K || searchValue(root.getLeft(), K)) {
+            return true;
+        }
+        return searchValue(root.getRight(), K);
+    }
+}
